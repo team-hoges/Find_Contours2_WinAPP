544ec6b91e65da837528724f455fe08574a12e4b
keigotakamura <s15528salesio@outlook.jp>
2020-01-23 03:26:08 +0900

    基本実装おわおわり

--- Find_Contours2/img_read.cpp
+++ Find_Contours2/img_read.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <memory>
+#include<stdexcept>
+#include<Windows.h>
 #include <opencv2/core.hpp>
 #include <opencv2/opencv.hpp>
 #include <opencv2/highgui.hpp>
@@ -13,34 +16,104 @@
 #include<random>
 #include<strstream>
 #include <fstream>
-
+#include <filesystem>
 
 #define max_size 100
 #define max_pro 100
 using namespace cv;
 using namespace std;
+namespace fs = std::filesystem;
 
 
-vector<Mat>  Paper_Reed(string Paper_name) {
 
-	vector<Mat> Papers;
-	vector<string>file_names;
-	string Paper_num = "0";
-	string num;
 
-	Mat sta_img;
+vector<Mat>  Paper_Reed(vector<string> Paper_name) {//ファイル読み込み
+	vector<Mat> img_datas;
+	Mat out_mat;
+	for (auto Paper_data : Paper_name) {
+		cout << Paper_data << endl;
+		out_mat = imread(Paper_data);
 
-	for (size_t i = 0; i < max_pro; i++) {
-		num = to_string(i);
-		Paper_name = num + ".jpg";//ファイル名指定
-		sta_img = imread(Paper_name);
-		if (true == sta_img.empty()) {
-			if (i == 0) {
-				break;
-			}
-		
-			Papers.push_back(imread(Paper_name));
+		if (out_mat.empty()) {
+			cout << "read Error!!!" << endl;
+			exit(1);
 		}
-		return Papers;
+
+		img_datas.push_back(out_mat);
 	}
+
+	return img_datas;
+}
+
+
+int Paper_output(vector<vector<Mat>> out) {//ファイル出力.
+	string Folder_names="./Regular_test/";
+	string ser_name;
+	string img_data_name;
+	//string current_paths = fs::current_path();
+	vector < vector<Mat> >out_ans;
+	int een=0;
+	//fs::path ans_path=fs::current_path();//作業パスの出力
+	fs::path out_path;
+
+
+	for (auto a: out) {
+		if (een < a.size()) {//一番大問数が多い問題を念の為取得
+			een = a.size();
+		}
+	}
+
+	
+
+	for (size_t i = 0; i < INT_FAST32_MAX; i++)
+	{
+		if (true != fs::exists(Folder_names + "/version" + to_string(i))) {
+			fs::create_directories(Folder_names + "/version" + to_string(i));//ディレクトリ作成
+			fs::current_path(Folder_names + "/version" + to_string(i));
+			break;
+		}
+	}
+
+	
+
+	for (size_t i = 0; i < out.size(); i++)
+	{
+
+		
+		for (size_t j = 0; j < out.at(i).size(); j++) {
+
+			if (false == fs::exists("./test_" + to_string(j))) {
+				fs::create_directories("./test_" + to_string(j));
+			}
+			fs::current_path("./test_" + to_string(j));
+
+			//fs::path testout = fs::absolute(fs::current_path());
+			imwrite(fs::absolute(fs::current_path()).string() +"/" +to_string(i) + ".jpg",out.at(i).at(j));
+			fs::current_path("../");
+		
+		}
+		
+	}
+
+	
+
+
+
+
+	return 0;
+}
+
+
+
+vector<vector<Mat>> sort_img(vector<vector<Mat>> imgs) {
+	Mat hsv_test;
+	for (auto test : imgs) {
+		for (auto cvt : imgs) {
+			cvtColor(cvt, hsv_test, CV_BGR2HSV);
+			
+		 }
+	}
+
+
+	return imgs;
 }
--- Find_Contours2/main.cpp
+++ Find_Contours2/main.cpp
@@ -16,57 +16,78 @@
 #include<random>
 #include<strstream>
 #include <fstream>
-
+#include <filesystem>
 
 #define max_size 100
 #define max_pro 100
+#define input_dir_name input
 using namespace cv;
 using namespace std;
 
 //Windows仕様コード
-int img_run(Mat image1);
-vector<Mat>  Paper_Reed();
+vector<Mat> img_run(Mat image1,string filename);//画像処理関数
+vector<Mat>  Paper_Reed(vector<string> Paper_name);//画像読み込み関数
+vector<vector<Mat>> sort_img(vector<vector<Mat>> imgs);
+int  Paper_output(vector<vector<Mat>> out);
 Point max(vector<Point> P);
 Point min(vector<Point> P);
 
+
+namespace fs = std::filesystem;
 
 int main() {
 	//string read_file = "./input_file/";
 	HANDLE hFind;
 	int per_num = 0;//答案枚数
-	string dir_name ="./input_file/";
+	int i = 0;
+	string dir_name ="./input/";
 	vector<string> read_files;
-	WIN32_FIND_DATA win32fd;
+
+	vector<Mat> Pre_processing_data;
+	vector < vector<Mat> >out_ans;
+	
 	vector<string> file_name;
 	std::string extension[3] = { "png" ,"jpg", "bmp" };
 
+	Pre_processing_data=Paper_Reed(file_name);//画像読み込み.
 
-	for (size_t i = 0; i < 3; i++)
-	{
-		string ser_name=dir_name+ "*." + extension[i];
-		
-//		cout << ser_name << endl;
+	bool files_know = fs::exists("input");
 
-		hFind = FindFirstFile(ser_name.c_str(), &win32fd);
-		if (hFind == INVALID_HANDLE_VALUE) {
-			continue;
-		}do {
-			if (win32fd.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) {
-			}
-			else {
-				file_name.push_back(win32fd.cFileName);
-			}
-		} while (FindNextFile(hFind, &win32fd));
-
-		FindClose(hFind);
+	if (files_know == false) {
+		filesystem::create_directory("input");
 	}
+
+	
+	string ser_name = dir_name + "*." + extension[i];
+
+	for (const fs::directory_entry& x : fs::recursive_directory_iterator(dir_name)) {//ファイル検索
+		
+		file_name.push_back(x.path().string());
+	}
+	
+	
 
 	cout << file_name.size() << endl;
 	per_num = file_name.size();
+	Pre_processing_data = Paper_Reed(file_name);
+
+	i = 0;
+	for (auto imgs : Pre_processing_data) {
+		out_ans.push_back(	img_run(imgs,file_name.at(i)));
+		i++;
+	}
 
 
 	
+	Paper_output(out_ans);//一時的にoutput;
+
+
+
+
+
 	
+
+
 	return 0;
 }
 
